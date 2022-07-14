a39e82b8ae5c98853fe2e505e36caad016cc9a25
Bethuel Dela Cruz <jbethueldc@gmail.com>
2022-07-14 08:27:13 +0800

    environment variables in index.html

--- src/config/environmentVariables.ts
+++ src/config/environmentVariables.ts
@@ -1,5 +1,5 @@
 type Build = 'prod' | 'dev';
 
 export const environmentVariables = {
-  SAMPLE_ENV: process.env.SAMPLE_ENV as Build,
+  ENVIRONMENT: process.env.ENVIRONMENT as Build,
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslintPlugin from 'vite-plugin-eslint';
+import { defineConfig } from 'vite';
 import environmentPlugin from 'vite-plugin-environment';
+import eslintPlugin from 'vite-plugin-eslint';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), eslintPlugin(), environmentPlugin(['BUILD'])],
+export default defineConfig(() => {
+  const ENVIRONMENT = 'ENVIRONMENT';
+
+  return {
+    plugins: [
+      react(),
+      eslintPlugin(),
+      environmentPlugin([ENVIRONMENT]),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            [ENVIRONMENT]: process.env[ENVIRONMENT],
+          },
+        },
+      }),
+    ],
+  };
 });
